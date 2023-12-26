55b53f961f8488205fc5530dd59c24d842d11043
Mohamed Adel <mohamed101291@gmail.com>
2023-12-26 20:21:29 +0200

    express

--- 2-NPM/app.js
+++ 2-NPM/app.js
@@ -1,4 +1,8 @@
 var _ = require("underscore");
+// "underscore": "^1.13.6"   >> "^Major,Minor,Patch"
+// "^1.13.6"  === "1.x"
+// "~1.13.6" === "1.13.x"
+// "1.13.6" === "1.13.6" only with no updated versions
 
 var result = _.contains([1, 2, 3], 2);
 console.log(result);
