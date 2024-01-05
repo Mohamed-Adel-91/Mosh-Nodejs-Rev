8e9fdd15d442717c5c9abb4c176ad2a5b00be665
MohamedAdel <mohamed101291@gmail.com>
2024-01-05 19:39:12 +0200

    callback

---
+++ 5-Async/index.js
@@ -0,0 +1,22 @@
+console.log("before");
+getUser(1, (user) => {
+    console.log(user);
+    getRepo(user.githubUserName, (repo) => {
+        console.log(`${user.githubUserName} is working on ${repo}`);
+    });
+});
+
+console.log("after");
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log("reading users from database ...!");
+        callback({ id: id, githubUserName: "mohamed-adel-91" });
+    }, 2000);
+}
+
+function getRepo(username, callback) {
+    setTimeout(() => {
+        callback(["repo1", "repo2", "repo3"]);
+    }, 3000);
+}
