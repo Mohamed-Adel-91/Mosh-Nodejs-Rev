4d06a32c461abe2960bf785cbea30cfb78a8bc41
Mohamed Adel <mohamed101291@gmail.com>
2024-01-06 18:45:34 +0200

    .all & .race

--- 5-Asynchronous-JavaScript/promise-api.js
+++ 5-Asynchronous-JavaScript/promise-api.js
@@ -9,15 +9,15 @@ pp.catch((err) => console.log(err.message));
 const p1 = new Promise((resolve) => {
     setTimeout(() => {
         console.log("Async operation 1 ...!");
-        resolve("promise 1");
-    });
+        resolve(1);
+    }, 2000);
 });
 
 const p2 = new Promise((resolve) => {
     setTimeout(() => {
-        console.log("Async operation 2 ....");
-        resolve("promise 2");
-    });
+        console.log("Async operation 2 ...!");
+        resolve(2);
+    }, 2000);
 });
 
 Promise.all([p1, p2])
