3a344bd494aea9c9b59678ac013b0efb4ef33c49
MohamedAdel <mohamed101291@gmail.com>
2024-01-07 14:11:35 +0200

    creating model and saving document

--- 6-mongodb/index.js
+++ 6-mongodb/index.js
@@ -7,3 +7,28 @@ mongoose
     .connect("mongodb://localhost/playground")
     .then(() => dbDebugger("Connecting to the database ....!!"))
     .catch((err) => dbDebuggerErr(err));
+
+//in relational database sql we have >> (tables , rows) but in nonrelational database noSql we have (collection , documents)
+// create schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
+
+// create model
+async function createCourse() {
+    const Course = mongoose.model("course", courseSchema);
+    const course = new Course({
+        name: "Building React Applications",
+        author: "Mike Williams",
+        tags: ["react.js", "javascript"],
+        isPublished: true,
+    });
+
+    const result = await course.save();
+    startupDebugger(result);
+}
+createCourse();
