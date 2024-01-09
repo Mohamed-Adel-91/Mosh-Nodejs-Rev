ceaddc8479746eda893645c1ebfd924a6457a3af
Mohamed Adel <mohamed101291@gmail.com>
2024-01-09 18:31:46 +0200

    update and delete data

--- 6-mongodb/index.js
+++ 6-mongodb/index.js
@@ -33,28 +33,30 @@ async function createCourse() {
 createCourse();
 
 // Query Document and how to get and filter something in course document
+// filtering in mongodb > comparison query operation
+// eq >> Equal
+// ne >> not Equal
+// gt >> Greater than
+// gte >> greater Than or Equal to
+// lt >> less than
+// lte >> Less Than or Equal to
+// in
+// nin >> Not In
+// filtering in mongodb >> logical query operation
+// and
+// or
+// filtering in mongodb >> regular expressions
+//start with  >> .find({author: /^Mosh/}) >> case sensitive
+//start with  >> .find({author: /^Mosh/i})>> case insensitive
+// ends with >> .find({author: /Mosh$/}) >> case sensitive
+// ends with >> .find({author: /Mosh$/i}) >> case insensitive
+// contain something >> .find({author: /.*Mosh.*/}) >> case sensitive
+// contain something >> .find({author: /.*Mosh.*/i})>> case insensitive
+//.count(); >> to show result of count of documents
+// pagination >> .skip(); >>   we can find ex. in line
+
+//getData
 async function getCourses() {
-    // filtering in mongodb > comparison query operation
-    // eq >> Equal
-    // ne >> not Equal
-    // gt >> Greater than
-    // gte >> greater Than or Equal to
-    // lt >> less than
-    // lte >> Less Than or Equal to
-    // in
-    // nin >> Not In
-    // filtering in mongodb >> logical query operation
-    // and
-    // or
-    // filtering in mongodb >> regular expressions
-    //start with  >> .find({author: /^Mosh/}) >> case sensitive
-    //start with  >> .find({author: /^Mosh/i})>> case insensitive
-    // ends with >> .find({author: /Mosh$/}) >> case sensitive
-    // ends with >> .find({author: /Mosh$/i}) >> case insensitive
-    // contain something >> .find({author: /.*Mosh.*/}) >> case sensitive
-    // contain something >> .find({author: /.*Mosh.*/i})>> case insensitive
-    //.count(); >> to show result of count of documents
-    // pagination >> .skip(); >>   we can find ex. in line
     const pageNumber = 2;
     const pageSize = 10;
     // /api/courses?pageNumber=2&pageSize=10
@@ -74,3 +76,42 @@ async function getCourses() {
     dbDebugger(courses);
 }
 getCourses();
+/**
+ * when updating data we have two kind of handling >>
+ *
+ * 1- approach : Query first
+ *  - .findById()
+ *  - modify its properties
+ *  - save()
+ *
+ * 2- approach : update first
+ * - update directly
+ * -optionally : get the updated document
+ *
+ */
+//update data
+async function updateCourses(id) {
+    const course = await Course.findById(id);
+    if (!course) return;
+    // - modify its properties >> we have two ways
+    course.isPublished = true;
+    course.author = "Mohamed Adel";
+    // second way
+    // course.set({
+    //     isPublished: true,
+    //     author: "Mohamed Adel"
+    // });
+    const result = await course.save();
+    dbDebugger(result);
+}
+updateCourses("659d67e09c667d76917e2b5f");
+
+// Remove data
+// .deleteOne()
+// .deleteMany()
+// .findByIdAndRemove()
+async function removeData(id) {
+    const result = await Course.deleteOne({ _id: id });
+    console.log(result);
+}
+removeData();
